0a8239c311ab1752bb7496c464f3950d7fc582d4
Jens Carlberg <jens.carlberg@liu.se>
2022-11-18 08:45:44 +0100

    Files for 2021 18, 19 and 20

---
+++ src/main/java/se/liu/jenca01/adventofcode/y2021/Dec18.java
@@ -0,0 +1,162 @@
+package se.liu.jenca01.adventofcode.y2021;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.tuple.MutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+import se.liu.jenca01.adventofcode.Christmas;
+
+public class Dec18 extends Christmas {
+
+    long sampleAnswer1 = 4140; // [[[[6,6],[7,6]],[[7,7],[7,0]]],[[[7,7],[7,7]],[[7,8],[9,9]]]]
+    long sampleAnswer2 = 0;
+
+    public static void main(String[] args) throws Exception {
+        var christmas = new Dec18();
+        christmas.solveSample();
+        christmas.solveMy();
+    }
+
+    @Override
+    public void solveSample() throws Exception {
+        long start = System.currentTimeMillis();
+        long solve1 = solve1(sampleData());
+        long stop = System.currentTimeMillis();
+        System.out.println("Example solve1 timing: " + (stop-start) + "ms");
+        assertEquals(sampleAnswer1, solve1, "solve1 is expected to return " + sampleAnswer1);
+
+        start = System.currentTimeMillis();
+        long solve2 = solve2(sampleData());
+        stop = System.currentTimeMillis();
+        System.out.println("Example solve2 timing: " + (stop-start) + "ms");
+        assertEquals(sampleAnswer2, solve2, "solve2 is expected to return " + sampleAnswer2);
+    }
+
+    @Override
+    public void solveMy() throws Exception {
+        System.out.println(simpleClassName() + " solve1: " + solve1(myData()));
+        System.out.println(simpleClassName() + " solve2: " + solve2(myData()));
+    }
+
+    private List<MutablePair<Object, Object>> convertData(Stream<String> data) {
+        var lines = toList(data);
+        var nums = new ArrayList<MutablePair<Object, Object>>();
+        for (var line: lines)
+            nums.add(parseNum(line));
+        return nums;
+    }
+
+    private MutablePair<Object, Object> parseNum(String line) {
+        line = line.replace('[', '(').replace(']',')'); // borde ge num.equals(parseNum(num.toString)) == true
+        var stack = new Stack<MutablePair<Object, Object>>();
+        var currPair = new MutablePair<Object, Object>();
+        for (var c: line.toCharArray()) {
+            switch (c) {
+                case '(':
+                    stack.push(currPair);
+                    currPair = new MutablePair<>();
+                    break;
+                case ')':
+                    var prevPair = stack.pop();
+                    if (prevPair.getLeft() == null)
+                        prevPair.left = currPair;
+                    else
+                        prevPair.right = currPair;
+                    currPair = prevPair;
+                    break;
+                case ',':
+                    break;
+                default:
+                    int numVal = c - '0';
+                    if (currPair.getLeft() == null) currPair.left = numVal;
+                    else currPair.right = numVal;
+            }
+        }
+        return (MutablePair<Object, Object>) currPair.getLeft();
+    }
+
+    public long solve1(Stream<String> stream) {
+        var data = convertData(stream);
+        MutablePair<Object, Object> added = data.remove(0);
+        for (var num: data) {
+            added = new MutablePair<>(added, num);
+            while (reduce(added)) {}
+        }
+        return 0;
+    }
+
+    private boolean reduce(MutablePair<Object, Object> added) {
+        var parents = new Stack<MutablePair<Object, Object>>();
+        if (explode(added, 0, parents)) return true;
+        if (split(added)) return true;
+        return false;
+    }
+
+    private boolean explode(MutablePair<Object, Object> added, int depth, Stack<MutablePair<Object,Object>> parents) {
+        if (added.left instanceof Pair) {
+            parents.add((MutablePair<Object, Object>) added.left);
+            if (explode((MutablePair<Object, Object>) added.left, depth+1, parents))
+                return true;
+            parents.pop();
+        }
+
+        if (added.right instanceof Pair) {
+            parents.add((MutablePair<Object, Object>) added.right);
+            if (explode((MutablePair<Object, Object>) added.right, depth+1, parents))
+                return true;
+            parents.pop();
+        }
+
+        if (depth < 4) return false;
+
+        return false;
+    }
+
+    private List<Object> flatten(MutablePair<Object, Object> added, List<Object> list) {
+        if (added.left instanceof Pair)
+            flatten((MutablePair<Object, Object>) added.left, list);
+        else
+            list.add(added.left);
+
+        if (added.right instanceof Pair)
+            flatten((MutablePair<Object, Object>) added.right, list);
+        else
+            list.add(added.right);
+
+        return list;
+    }
+
+    private boolean split(MutablePair<Object, Object> added) {
+        if (added.getLeft() instanceof Pair)
+            if (split((MutablePair<Object, Object>) added.getLeft()))
+                return true;
+        var left = (int) added.getLeft();
+        if (left > 9) {
+            added.left = new MutablePair<Object, Object>(left/2, (left+1)/2);
+            return true;
+        }
+        
+        if (added.getRight() instanceof Pair)
+            if (split((MutablePair<Object, Object>) added.getRight()))
+                return true;
+        var right = (int) added.getRight();
+        if (right > 9) {
+            added.right = new MutablePair<Object, Object>(right/2, (right+1)/2);
+            return true;
+        }
+        return false;
+    }
+
+    public long solve2(Stream<String> stream) {
+        return 0;
+    }
+}
+
+/*
+
+
+
+ */
---
+++ src/main/java/se/liu/jenca01/adventofcode/y2021/Dec20.java
@@ -0,0 +1,117 @@
+package se.liu.jenca01.adventofcode.y2021;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.apache.commons.lang3.tuple.Pair;
+import se.liu.jenca01.adventofcode.Christmas;
+
+public class Dec20 extends Christmas {
+
+    long sampleAnswer1 = 35;
+    long sampleAnswer2 = 0;
+
+    class PuzzleInput {
+
+        private String algo;
+        private Set<Pair<Integer, Integer>> points;
+        private int maxX;
+        private int maxY;
+
+        public PuzzleInput(String algo, Set<Pair<Integer, Integer>> points, int maxX, int maxY) {
+            this.algo = algo;
+            this.points = points;
+            this.maxX = maxX;
+            this.maxY = maxY;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        var christmas = new Dec20();
+        christmas.solveSample();
+        christmas.solveMy();
+    }
+
+    @Override
+    public void solveSample() throws Exception {
+        long start = System.currentTimeMillis();
+        long solve1 = solve1(sampleData());
+        long stop = System.currentTimeMillis();
+        System.out.println("Example solve1 timing: " + (stop-start) + "ms");
+        assertEquals(sampleAnswer1, solve1, "solve1 is expected to return " + sampleAnswer1);
+
+        start = System.currentTimeMillis();
+        long solve2 = solve2(sampleData());
+        stop = System.currentTimeMillis();
+        System.out.println("Example solve2 timing: " + (stop-start) + "ms");
+        assertEquals(sampleAnswer2, solve2, "solve2 is expected to return " + sampleAnswer2);
+    }
+
+    @Override
+    public void solveMy() throws Exception {
+        System.out.println(simpleClassName() + " solve1: " + solve1(myData()));
+        System.out.println(simpleClassName() + " solve2: " + solve2(myData()));
+    }
+
+    private PuzzleInput convertData(Stream<String> data) {
+        var lines = toList(data);
+        var algo = lines.remove(0);
+        lines.remove(0);
+        var points = new HashSet<Pair<Integer, Integer>>();
+
+        for (int y=0; y<lines.size(); y++)
+            for(int x=0; x<lines.get(y).length(); x++)
+                if (lines.get(y).charAt(x) == '#')
+                    points.add(Pair.of(x, y));
+        
+        return new PuzzleInput(algo, points, lines.size(), lines.get(0).length());
+    }
+
+    public long solve1(Stream<String> stream) {
+        var data = convertData(stream);
+        
+        for (int i=0; i<2; i++)
+            // TODO: take care of that all points outside will be lit after iter 1 (and u lit after 2
+            data.points = enhance(data, i);
+        return data.points.size();
+    }
+
+    private Set<Pair<Integer, Integer>> enhance(PuzzleInput data, int i) {
+        var newPoints = new HashSet<Pair<Integer, Integer>>();
+        for(int x=0-i; x<data.maxX+i; x++)
+            for(int y=0-i; y<data.maxY+i; y++) {
+                int calcIndex = calcIndex(data, x, y, i);
+                char charAt = data.algo.charAt(calcIndex);
+                if (charAt == '#')
+                    newPoints.add(Pair.of(x, y));
+            }
+        return newPoints;
+    }
+
+    private int calcIndex(PuzzleInput data, int x, int y, int i) {
+        StringBuilder sb = new StringBuilder(9);
+        for(int dy=-1; dy<2; dy++)
+            for(int dx=-1; dx<2; dx++)
+                sb.append(translatePoint(data, x+dx, y+dy, i));
+        return Integer.parseInt(sb.toString(), 2);
+    }
+
+    private char translatePoint(PuzzleInput data, int px, int py, int i) {
+        if (i % 2 == 1) {
+            if (px<-i || px >=data.maxX+i) return '1';
+            if (py<-i || py >=data.maxY+i) return '1';
+        }
+        return data.points.contains(Pair.of(px, py)) ? '1' : '0';
+    }
+
+    public long solve2(Stream<String> stream) {
+        return 0;
+    }
+}
+
+/*
+
+
+
+*/
