c8f784b20883a8cd819175860ddb8aeac7debd9d
Jens Carlberg <jens.carlberg@gmail.com>
2022-12-20 23:06:39 +0100

    Files and solution attempts for 2022-12-20.

--- src/main/java/se/liu/jenca01/adventofcode/y2022/Dec19.java
+++ src/main/java/se/liu/jenca01/adventofcode/y2022/Dec19.java
@@ -1,14 +1,18 @@
 package se.liu.jenca01.adventofcode.y2022;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
 import se.liu.jenca01.adventofcode.Christmas;
 
 public class Dec19 extends Christmas {
 
-    long sampleAnswer1 = 0;
+    long sampleAnswer1 = 33;
     long sampleAnswer2 = 0;
 
     public static void main(String[] args) throws Exception {
@@ -38,17 +42,123 @@ public class Dec19 extends Christmas {
         System.out.println(simpleClassName() + " solve2: " + solve2(myData()));
     }
 
-    private List<Long> convertData(Stream<String> data) {
-        return data.map(s -> Long.parseLong(s)).collect(Collectors.toList());
+    private List<Blueprint> convertData(Stream<String> data) {
+    	var lines = data.toList();
+    	var blueprints = new ArrayList<Blueprint>();
+    	for (var line: lines)
+    		blueprints.add(buildBlueprint(line));
+        return blueprints;
     }
 
-    public long solve1(Stream<String> stream) {
+    private Blueprint buildBlueprint(String line) {
+        Pattern r = Pattern.compile("Blueprint \\d+: Each ore robot costs (\\d+) ore. Each clay robot costs (\\d+) ore. Each obsidian robot costs (\\d+) ore and (\\d+) clay. Each geode robot costs (\\d+) ore and (\\d+) obsidian.");
+        Matcher m = r.matcher(line);
+
+        if (m.find( )) {
+        	var oreOre = parse(m.group(1));
+        	var clayOre = parse(m.group(2));
+        	var obsidianOre = parse(m.group(3));
+        	var obsidianClay = parse(m.group(4));
+        	var geodeOre = parse(m.group(5));
+        	var geodeObsidian = parse(m.group(6));
+        	return new Blueprint(
+        			Cost.ore(oreOre),
+        			Cost.clay(clayOre),
+        			Cost.obsidian(obsidianOre, obsidianClay),
+        			Cost.geode(geodeOre, geodeObsidian));
+        }
+        throw new RuntimeException("Line doews not match.");
+	}
+
+	private int parse(String num) {
+		return Integer.parseInt(num);
+	}
+
+	public long solve1(Stream<String> stream) {
+    	var noOfMinutes = 24;
+    	var blueprints = convertData(stream);
+    	var geodes = new ArrayList<Integer>();
+    	for (var blueprint: blueprints)
+    		geodes.add(simulate(blueprint, noOfMinutes));
+    	var sum = 0;
+    	for (var geode=0; geode<geodes.size(); geode++)
+    		sum += (geode + 1)*geodes.get(geode);
+        return sum;
+    }
+
+    private Integer simulate(Blueprint blueprint, int noOfMinutes) {
+    	var ore = 0;
+    	var clay = 0;
+    	var obsidian = 0;
+    	var geode = 0;
+    	var oreRobots = 1;
+    	var clayRobots = 0;
+    	var obsidianRobots = 0;
+    	var geodeRobots = 0;
+    	var factoryQueue = new ArrayList<RobotType>();
+
+    	for (var i=0; i<noOfMinutes; i++) {
+    		if (blueprint.geode.enoughFor(ore, clay, obsidian)) {
+    			factoryQueue.add(RobotType.GEODE);
+    			ore -= blueprint.geode.ore;
+    			clay -= blueprint.geode.clay;
+    			obsidian -= blueprint.geode.obsidian;
+    		}
+    		if (blueprint.obsidian.enoughFor(ore, clay, obsidian)) {
+    			factoryQueue.add(RobotType.OBSIDIAN);
+    			ore -= blueprint.obsidian.ore;
+    			clay -= blueprint.obsidian.clay;
+    			obsidian -= blueprint.obsidian.obsidian;
+    		}
+    		if (blueprint.clay.enoughFor(ore, clay, obsidian)) {
+    			factoryQueue.add(RobotType.CLAY);
+    			ore -= blueprint.clay.ore;
+    			clay -= blueprint.clay.clay;
+    			obsidian -= blueprint.clay.obsidian;
+    		}
+    		if (blueprint.ore.enoughFor(ore, clay, obsidian)) {
+    			factoryQueue.add(RobotType.ORE);
+    			ore -= blueprint.ore.ore;
+    			clay -= blueprint.ore.clay;
+    			obsidian -= blueprint.ore.obsidian;
+    		}
+    		ore += oreRobots;
+    		clay += clayRobots;
+    		obsidian += obsidianRobots;
+    		geode += geodeRobots;
+    		for (var type: factoryQueue) {
+    			switch (type) {
+				case CLAY: clayRobots++; break;
+				case GEODE: geodeRobots++; break;
+				case OBSIDIAN: obsidianRobots++; break;
+				case ORE: oreRobots++; break;
+    			}
+    		}
+    		System.out.println("== Minute " + (i+1) + " ==");
+    		System.out.println("ore:      " + oreRobots + " robots, " + ore + " ore");
+    		System.out.println("clay:     " + clayRobots + " robots, " + clay + " clay");
+    		System.out.println("obsidian: " + obsidianRobots + " robots, " + obsidian + " obsidian");
+    		System.out.println("geode:    " + geodeRobots + " robots, " + geode + " geode");
+    		factoryQueue.clear();
+    	}
+		return geode;
+	}
+
+	public long solve2(Stream<String> stream) {
         return 0;
     }
 
-    public long solve2(Stream<String> stream) {
-        return 0;
+	enum RobotType {
+		ORE, CLAY, OBSIDIAN, GEODE;
+	}
+    record Cost(int ore, int clay, int obsidian) {
+    	boolean enoughFor(int ore, int clay, int onbsidian) { return ore >= this.ore && clay >= this.clay && onbsidian >= obsidian; }
+    	static Cost ore(int ore) { return new Cost(ore, 0, 0); }
+    	static Cost clay(int ore) { return ore(ore); }
+    	static Cost obsidian(int ore, int clay) { return new Cost(ore, clay, 0); }
+    	static Cost geode(int ore, int obsidian) { return new Cost(ore, 0, obsidian); }
     }
+    record Blueprint(Cost ore, Cost clay, Cost obsidian, Cost geode) {}
 }
 
 /*
---
+++ src/main/java/se/liu/jenca01/adventofcode/y2022/Dec20.java
@@ -0,0 +1,140 @@
+package se.liu.jenca01.adventofcode.y2022;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.TreeSet;
+import java.util.stream.Stream;
+
+import se.liu.jenca01.adventofcode.Christmas;
+
+public class Dec20 extends Christmas {
+
+    long sampleAnswer1 = 3;
+    long sampleAnswer2 = 1623178306;
+
+    public static void main(String[] args) throws Exception {
+        var christmas = new Dec20();
+        christmas.solveSample();
+        christmas.solveMy();
+    }
+
+    @Override
+    public void solveSample() throws Exception {
+        long start = System.currentTimeMillis();
+        long solve1 = solve1(sampleData());
+        long stop = System.currentTimeMillis();
+        System.out.println("Example solve1 timing: " + (stop-start) + "ms");
+        assertEquals(sampleAnswer1, solve1, "solve1 is expected to return " + sampleAnswer1);
+
+        start = System.currentTimeMillis();
+        long solve2 = solve2(sampleData());
+        stop = System.currentTimeMillis();
+        System.out.println("Example solve2 timing: " + (stop-start) + "ms");
+        assertEquals(sampleAnswer2, solve2, "solve2 is expected to return " + sampleAnswer2);
+    }
+
+    @Override
+    public void solveMy() throws Exception {
+        System.out.println(simpleClassName() + " solve1: " + solve1(myData()));
+        System.out.println(simpleClassName() + " solve2: " + solve2(myData()));
+    }
+
+    private List<Num> convertData(Stream<String> data, long multiplier) {
+    	var puzzle = new ArrayList<Num>();
+    	var lines = data.toList();
+    	for (var i=0; i<lines.size(); i++)
+    		puzzle.add(new Num(i, multiplier * Integer.parseInt(lines.get(i))));
+    	return puzzle;
+    }
+
+    public long solve1(Stream<String> stream) {
+    	var puzzle = convertData(stream, 1);
+    	var mixed = new ArrayList<Num>();
+    	mixed.addAll(puzzle);
+    	var ms = new TreeSet<Num>();
+    	ms.addAll(puzzle);
+    	if (ms.size() != mixed.size()) {
+    		for (var i: ms)
+    			mixed.remove(mixed.indexOf(i));
+    		System.out.println(mixed);
+    		throw new RuntimeException("Found doubles...");
+    	}
+    	for(var num: puzzle) {
+    		if (num.value == 0) continue;
+    		var pos = mixed.indexOf(num);
+    		mix(pos, mixed);
+    	}
+    	var pos0 = 0;
+    	for (; pos0 < mixed.size(); pos0++) {
+    		if (mixed.get(pos0).value == 0)
+    			break;
+    	}
+        int pos1 = (pos0 + 1000) % mixed.size();
+        int pos2 = (pos0 + 2000) % mixed.size();
+        int pos3 = (pos0 + 3000) % mixed.size();
+        long val1 = mixed.get(pos1).value;
+        long val2 = mixed.get(pos2).value;
+        long val3 = mixed.get(pos3).value;
+		return val1 + val2 + val3;
+    }
+
+    private void mix(int pos, ArrayList<Num> mixed) {
+    	Num posNum = mixed.get(pos);
+		var posVal = posNum.value;
+    	var newPos = (int)(pos + posVal);
+    	while (newPos < 0) newPos += mixed.size() - 1;
+    	while (newPos >= mixed.size()) newPos -= mixed.size() - 1;
+    	if (newPos == 0) {
+    		mixed.add(posNum);
+    		mixed.remove(pos);
+    	} else if (pos < newPos) {
+    		mixed.add(newPos + 1, posNum);
+    		mixed.remove(pos);
+    	} else {
+    		mixed.remove(pos);
+    		mixed.add(newPos, posNum);
+    	}
+	}
+
+	public long solve2(Stream<String> stream) {
+    	var puzzle = convertData(stream, 811589153);
+    	var mixed = new ArrayList<Num>();
+    	mixed.addAll(puzzle);
+    	var ms = new TreeSet<Num>();
+    	for (int i=0; i<10; i++)
+    		for(var num: puzzle) {
+    			if (num.value == 0) continue;
+    			var pos = mixed.indexOf(num);
+    			mix(pos, mixed);
+    		}
+    	var pos0 = 0;
+    	for (; pos0 < mixed.size(); pos0++) {
+    		if (mixed.get(pos0).value == 0)
+    			break;
+    	}
+        int pos1 = (pos0 + 1000) % mixed.size();
+        int pos2 = (pos0 + 2000) % mixed.size();
+        int pos3 = (pos0 + 3000) % mixed.size();
+        long val1 = mixed.get(pos1).value;
+        long val2 = mixed.get(pos2).value;
+        long val3 = mixed.get(pos3).value;
+		return val1 + val2 + val3;
+    }
+
+	record Num(int pos, long value) implements Comparable<Num> {
+		@Override public int compareTo(Num o) {
+			return Objects.compare(this, o, Comparator.comparing(Num::pos).thenComparing(Num::value));
+		}
+		@Override public String toString() { return String.format("{%s: %s}", pos, value); }
+	}
+}
+
+/*
+
+
+
+*/
