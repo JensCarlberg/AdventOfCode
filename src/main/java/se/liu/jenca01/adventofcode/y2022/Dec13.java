bb844492358175459b0f98477b61833cbebe0f5a
Jens Carlberg <jens.carlberg@gmail.com>
2022-12-30 09:00:06 +0100

    Solution for 2022-12-13.

--- src/main/java/se/liu/jenca01/adventofcode/y2022/Dec13.java
+++ src/main/java/se/liu/jenca01/adventofcode/y2022/Dec13.java
@@ -1,16 +1,18 @@
 package se.liu.jenca01.adventofcode.y2022;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
+import java.util.TreeSet;
 import java.util.stream.Stream;
+
 import se.liu.jenca01.adventofcode.Christmas;
 
 public class Dec13 extends Christmas {
 
     long sampleAnswer1 = 13;
-    long sampleAnswer2 = 0;
+    long sampleAnswer2 = 140;
 
     public static void main(String[] args) throws Exception {
         var christmas = new Dec13();
@@ -40,8 +42,12 @@ public class Dec13 extends Christmas {
     }
 
     private List<Pair> convertData(Stream<String> data) {
-        var pairs = new ArrayList<Pair>();
-        var lines = data.toList();
+    	var lines = data.toList();
+        return convertData(lines);
+    }
+
+	private List<Pair> convertData(List<String> lines) {
+		var pairs = new ArrayList<Pair>();
         Packet left = null;
         for(var line: lines) {
             if (line.strip().length() == 0) continue;
@@ -53,80 +59,140 @@ public class Dec13 extends Christmas {
             }
         }
         return pairs;
-    }
+	}
 
     private Packet buildPacket(String packetDef) {
-        var packets = new Stack<Packet>();
-        Packet packet;
-        for (var c: packetDef.toCharArray()) {
+    	var symbols = new ArrayList<Character>();
+    	for (var c: packetDef.toCharArray())
+    		symbols.add(c);
+    	if (symbols.remove(0) != '[') throw new RuntimeException();
+    	if (symbols.remove(symbols.size() - 1) != ']') throw new RuntimeException();
+    	Packet packet = Packet.list(buildPacket(symbols));
+		return packet;
+    }
+
+    private List<Packet> buildPacket(List<Character> symbols) {
+        var packetList = new ArrayList<Packet>();
+        Packet packet = null;
+        while (symbols.size() > 0) {
+        	var c = symbols.remove(0);
             switch (c) {
-                case '[': {
-                    // börja samla tecken
-                    break;
-                }
-                case ']': {
-                    break;
-                }
-                default:
-                    throw new IllegalArgumentException("Unexpected value: " + c);
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+            	packet = (packet == null) ? Packet.leaf(c - '0') : Packet.leaf(10*packet.leaf + c - '0');
+            	break;
+            case '[':
+            	List<Packet> buildPacket = buildPacket(symbols);
+				Packet list = Packet.list(buildPacket);
+				packetList.add(list);
+            	break;
+            case ']':
+            	if (packet != null)
+            		packetList.add(packet);
+            	return packetList;
+            case ',':
+            	if (packet != null)
+            		packetList.add(packet);
+            	packet = null;
+            	break;
+            default:
+            	throw new RuntimeException("Unknown symbol: " + c);
             }
         }
-        // TODO Auto-generated method stub
-        return null;
+        if (packet != null)
+        	packetList.add(packet);
+        return packetList;
     }
 
     public long solve1(Stream<String> stream) {
-        return 0;
+    	var puzzle = convertData(stream);
+    	var orderIndexSum = 0;
+    	for (int i = 0; i<puzzle.size(); i++) {
+    		var pair = puzzle.get(i);
+    		Packet left = pair.left;
+			Packet right = pair.right;
+			if (left.compareTo(right) < 0)
+    			orderIndexSum += i + 1;
+    	}
+        return orderIndexSum;
     }
 
     public long solve2(Stream<String> stream) {
-        return 0;
+    	var puzzle = convertData(stream);
+    	var packets = new TreeSet<Packet>();
+    	for (var p:puzzle) {
+    		packets.add(p.left);
+    		packets.add(p.right);
+    	}
+    	var divider1 = buildPacket("[[2]]");
+    	var divider2 = buildPacket("[[6]]");
+    	packets.add(divider1);
+    	packets.add(divider2);
+    	var sortedList = packets.stream().toList();
+    	var decoderKey = (sortedList.indexOf(divider1) + 1L) * (sortedList.indexOf(divider2) + 1L);
+        return decoderKey;
     }
 
 
     record Pair(Packet left, Packet right) {}
-    record Packet(List<Packet> packets, int leaf) implements Comparable<Packet> {
+    static class Packet implements Comparable<Packet> {
+    	private List<Packet> packets;
+		private Integer leaf;
 
-        boolean isLeaf() { return packets == null || packets.size() == 0; }
+		Packet(List<Packet> packets, Integer leaf) {
+			this.packets = packets;
+			this.leaf = leaf;
+		}
 
-        @Override
-        public int compareTo(Packet right) {
-            var left = this;
-            if (left.isLeaf())
-                return compareLeaf(left.leaf, right);
-            if (right.isLeaf())
-                return -compareLeaf(right.leaf, left);
-            var rightSize = right.packets.size();
-            for(var i = 0; i<left.packets.size(); i++) {
-                if (i == rightSize) return 1;
-                var compRes = left.packets.get(i).compareTo(right.packets.get(i));
-                if (compRes != 0) return compRes;
-            }
-            return left.packets.size() - right.packets.size(); 
-        }
-        
-        int compareLeaf(int left, Packet right) {
-            if (right.isLeaf())
-                return left - right.leaf;
-            var list = new ArrayList<Packet>();
-            list.add(new Packet(null, left));
-            var leftAsPacket = Packet.list(list);
-            return leftAsPacket.compareTo(right);
-        }
-        
-        @Override
+		boolean isLeaf() { return leaf != null; }
+
+		@Override
+		public int compareTo(Packet right) {
+			var left = this;
+			if (left.isLeaf())
+				return compareLeaf(left.leaf, right);
+			if (right.isLeaf())
+				return -compareLeaf(right.leaf, left);
+			var rightSize = right.packets.size();
+			for(var i = 0; i<left.packets.size(); i++) {
+				if (i == rightSize) return 1;
+				var compRes = left.packets.get(i).compareTo(right.packets.get(i));
+				if (compRes != 0) return compRes;
+			}
+			return left.packets.size() - right.packets.size();
+		}
+
+		int compareLeaf(int left, Packet right) {
+			if (right.isLeaf())
+				return left - right.leaf;
+			var list = new ArrayList<Packet>();
+			list.add(new Packet(null, left));
+			var leftAsPacket = Packet.list(list);
+			return leftAsPacket.compareTo(right);
+		}
+
+		@Override
         public String toString() {
-            if (isLeaf()) return "" + leaf;
-            var tos = new StringBuilder();
-            tos.append("[");
-            tos.append(String.join(",", packets.stream().map(s -> s.toString()).toList()));
-            tos.append("]");
-            return tos.toString();
+        	if (isLeaf()) return "" + leaf;
+        	if (packets.size() == 0) return "[]";
+        	var tos = new StringBuilder();
+        	tos.append("[");
+        	tos.append(String.join(",", packets.stream().map(s -> s.toString()).toList()));
+        	tos.append("]");
+        	return tos.toString();
         }
-        
-        static Packet list(List<Packet> packets) { return new Packet(packets, 0); }
-        static Packet leaf(int leaf) { return new Packet(null, leaf); }
-        
+
+		static Packet list(List<Packet> packets) { return new Packet(packets, null); }
+		static Packet leaf(int leaf) { return new Packet(null, leaf); }
+		static Packet empty() { return new Packet(null, null); }
     }
 }
 
